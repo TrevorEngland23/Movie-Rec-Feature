564f1a18bc830fd191d7b812cb7087f41932832b
TrevorEngland23 <trevor.england.523@gmail.com>
2025-06-10 08:07:08 -0700

    Http trigger GGSM: Recieves POST requests from flask. On to phase 2: storage and returning response

--- app.py
+++ app.py
@@ -20,7 +20,16 @@ def index():
 def select_movies():
     if request.method == 'POST':
         selected_genres = request.form.getlist('genres')
-        return render_template("select-liked-movies.html", selected_genres=selected_genres)
+        print(selected_genres)
+        azure_function_url = "http://localhost:7071/api/HttpTriggerGGSM"
+        payload = {"genres": selected_genres}
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(azure_function_url, json=payload, headers=headers)
+        if response.status_code == 200:
+            print("Genres sent successfully to Azure Function.")
+        else:
+            print(f"Failed to send genres. Status code: {response.status_code}")
+        return render_template("select-liked-movies.html", selected_genres=selected_genres, az_response=response.text)
     return render_template("select-liked-movies.html")
 
 @app.route('/recommend-movies')
--- azure_functions/function_app.py
+++ azure_functions/function_app.py
@@ -9,22 +9,23 @@ app = func.FunctionApp()
 def HttpTriggerGGSM(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    genres = None
+    if req.method == 'GET':
+        genres = req.params.get('genres')
+    
+    if not genres:
         try:
             req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+            genres = req_body.get('genres')
+        except Exception as e:
+            logging.error(f"Error parsing request body: {e}")
+            genres = None
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+        if genres:
+            logging.info(f"Received genres: {genres}")
+            return func.HttpResponse(f"Received genres: {genres}", status_code=200)
+        else:
+            return func.HttpResponse("No genres receieved. Pass genres in request body as JSON.", status_code=400)
 
 @app.route(route="HttpTriggerMovieRecs", auth_level=func.AuthLevel.ANONYMOUS)
 def HttpTriggerMovieRecs(req: func.HttpRequest) -> func.HttpResponse:
