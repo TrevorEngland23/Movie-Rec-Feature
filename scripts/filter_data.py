d633a91cf95708e05c2743d570faf874fd53250c
TrevorEngland23 <trevor.england.523@gmail.com>
2025-06-13 00:05:47 -0700

    redacted search criteria..

--- scripts/filter_data.py
+++ scripts/filter_data.py
@@ -7,15 +7,16 @@ filtered_df = df[df['vote_average'] >= 7.5]
 filtered_df = filtered_df.dropna(subset=['genres', 'title', 'vote_average', 'runtime'])
 filtered_df = filtered_df[filtered_df['runtime'] >= 30]
 filtered_df = filtered_df[filtered_df['original_language'].isin(['en', 'es'])]
+
 # Attempt to remove pornographic content from the dataset... will also run images to openAI in attempt to hide pornographic material in images.
 # Removed this line of code after creating the dataset to prevent foul language in source code. Only including these files to demonstrate how the dataset was cleaned and populated.
-porn_content_kws = r"porn|sex|xxx|nude|bdsm|orgy|anal|cum|blowjob|masturbation|gangbang|milf|dildo|slut|tit|boobs|ass|pussy"
 
-filtered_df = filtered_df[
-    ~filtered_df['title'].str.contains(porn_content_kws, case=False, na=False) &
-    ~filtered_df['overview'].str.contains(porn_content_kws, case=False, na=False) &
-    ~filtered_df['keywords'].str.contains(porn_content_kws, case=False, na=False)
-]
+# porn_content_kws =  REDACTED
+# filtered_df = filtered_df[
+#     ~filtered_df['title'].str.contains(porn_content_kws, case=False, na=False) &
+#     ~filtered_df['overview'].str.contains(porn_content_kws, case=False, na=False) &
+#     ~filtered_df['keywords'].str.contains(porn_content_kws, case=False, na=False)
+# ]
 print (filtered_df.head())
 filtered_df = filtered_df.sort_values(by='vote_average', ascending=False)
 print(len(filtered_df))
